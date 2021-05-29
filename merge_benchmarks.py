dae0a6a4aa4805e96dbb95d323abba1647988d10
Colin Catlin <colin.catlin@gmail.com>
2021-05-28 22:15:22 -0400

    more files

--- merge_benchmarks.py
+++ merge_benchmarks.py
@@ -6,8 +6,8 @@ file_list = {"11700intel": "general_template_11700_intelfullDESKTOP-JS3OJ8L.csv"
              # "11700intelOC": "general_template_11700intel_full_ocDESKTOP-JS3OJ8L.csv",
              "11700openblas": "general_template_11700openblasfullDESKTOP-JS3OJ8L.csv",
              "amd4500Uopenblas": "general_template_amd_fullcolin-pn50.csv",
-             "10700openblas": "general_template_openblasDESKTOP-JS3OJ8L.csv",
              "10700intel": "general_templateDESKTOP-JS3OJ8L.csv",
+             "10700openblas": "general_template_openblasDESKTOP-JS3OJ8L.csv",
              "1135g7openblas": "general_template_1135g7_openblascolin-1135.csv",
              "1135g7intel_cold": "general_template_1135g7_intel_coldcolin-1135.csv",
              "1135g7intel": "general_template_1135g7intel_fullcolin-1135.csv",
@@ -16,6 +16,7 @@ count = 0
 for key, value in file_list.items():
     df = pd.read_csv(value, index_col=0)
     df['TotalRuntimeException'] = np.where(df['Exceptions'].isna(), df['TotalRuntime'], pd.NA)
+    df = df[df['ValidationRound'] == 0]
     df_slice = df[['ID', 'Model','ModelParameters', 'TotalRuntimeException']]
     df_slice.columns = ["ID", "Model",'ModelParameters', key]
     if count == 0:
